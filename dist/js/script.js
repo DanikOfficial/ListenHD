e76938aa9684fe0be875084f322487ec86751955
Pete J. Chiboleca <pete9450@gmail.com>
2021-06-17 19:49:26 +0200

    Finished Buy Now, Get In Touch Sections and Started Footer

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+let currentImgName = "head-1";
+
+document.querySelector(".colors").addEventListener("click", (event) => {
+  const classList = Array.from(event.target.classList);
+  const classNames = classList.join(" ");
+
+  if (classNames.includes("head")) {
+    const imgName = classList[classList.length - 1] + ".png";
+
+    if (currentImgName !== imgName) {
+      const buttons = Array.from(event.target.parentNode.childNodes);
+
+      buttons.shift();
+      buttons.forEach((el) => el.classList.remove("selected"));
+
+      document
+        .querySelector("." + classList[classList.length - 1])
+        .classList.add("selected");
+
+      const imgLink = document.querySelector(".slide-next").src.split("/");
+      imgLink.pop();
+      imgLink.push(imgName);
+
+      document.querySelector(".slide-next").src = imgLink.join("/");
+      currentImgName = imgName;
+    }
+  }
+});
